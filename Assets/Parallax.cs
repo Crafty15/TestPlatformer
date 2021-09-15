6237d1e9b4bff0fc270d09fc123b21a61e2db88e
Liam Craft <crafty0410@gmail.com>
2021-09-15 21:18:16 +1000

    more of level2. Fixes parallax scrolling

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Parallax : MonoBehaviour{
-    private float length, startpos;
-    public GameObject camera;
+    private float startY, startX;
+    public GameObject follow_object;        //usually the camera
     public float parallaxEffect;
+    public bool lockY = false;
     // Start is called before the first frame update
     void Start(){
-        startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        startX = transform.position.x;
+        startY = transform.position.y;
+        //length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void FixedUpdate(){
         //how far we moved in world space
-        float dist = (camera.transform.position.x * parallaxEffect);
+        float dist = (follow_object.transform.position.x * parallaxEffect);
         //move camera
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        if (lockY) {
+            transform.position = new Vector3(startX + dist, startY, transform.position.z);
+        }
+        else {
+            transform.position = new Vector3(startX + dist, transform.position.y, transform.position.z);
+        }
+        
     }
 }
